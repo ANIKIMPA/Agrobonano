08d728d605b7131ba8a36f2c7f53ac88d5ebb4d8
ANIKIMPA <niovan.martinez9@gmail.com>
2019-10-01 07:33:30 -0400

    settings change

--- src/manuel_web/settings.py
+++ src/manuel_web/settings.py
@@ -165,16 +165,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 LOCAL_STATIC_CDN_PATH = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
-
 STATIC_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'static') # live cdn AWS S3
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
-]
-
 MEDIA_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'media')
-MEDIA_URL = '/media/'
 
 
 EMAIL_HOST = 'smtp.mailtrap.io'
