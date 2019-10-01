e849437ea91db6c7df162973b88230f89a786b53
ANIKIMPA <niovan.martinez9@gmail.com>
2019-10-01 14:46:59 -0400

    debug true

--- src/manuel_web/settings.py
+++ src/manuel_web/settings.py
@@ -165,15 +165,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/') # live cdn AWS S3
 
 LOCAL_STATIC_CDN_PATH = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
 
+STATIC_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'static') # live cdn AWS S3
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticFILES')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
-MEDIA_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'media/')
+MEDIA_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'media')
 MEDIA_URL = '/media/'
 
 
