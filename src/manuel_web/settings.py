72626dc76968a297355df33082ebe31adaad4aba
ANIKIMPA <niovan.martinez9@gmail.com>
2019-10-01 07:52:49 -0400

    debug true

--- src/manuel_web/settings.py
+++ src/manuel_web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1q%zz*1==q9f8qizynbncke8mci*&vw07024f5qzz1iyee8_2c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['104.248.12.122']
 
